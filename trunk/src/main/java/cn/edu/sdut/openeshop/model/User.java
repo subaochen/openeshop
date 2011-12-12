04bd1ed79ab2677cb5dbcf6f15fc28f3ee19bc73
subaochen@gmail.com <subaochen@gmail.com@25014077-2549-54d7-ebe6-9a0042c0366a>
2011-12-12 09:40:29 +0000

    修正数据库配置文件准备将user保存到数据库中

--- trunk/src/main/java/cn/edu/sdut/openeshop/model/User.java
+++ trunk/src/main/java/cn/edu/sdut/openeshop/model/User.java
@@ -2,47 +2,69 @@ package cn.edu.sdut.openeshop.model;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	private Long id;
 	private String username;
 	private String password;
-	private String rePassword;
 	private String email;
-	
-	public User(){
+
+	public User() {
 		this.username = "";
 		this.password = "";
-		this.rePassword = "";
 		this.email = "";
 	}
-	
-	public User(String username,String password){
+
+	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
-	
+
+	@Id
+	@SequenceGenerator(name = "user_seq", sequenceName = "user_id_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "username")
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
+	@Column(name = "password")
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getRePassword() {
-		return rePassword;
-	}
-	public void setRePassword(String rePassword) {
-		this.rePassword = rePassword;
-	}
+
+	@Column(name = "email")
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -50,6 +72,6 @@ public class User implements Serializable{
 	@Override
 	public String toString() {
 		return "User [username=" + username + ", password=" + password
-				+ ", rePassword=" + rePassword + ", email=" + email + "]";
+				+ ", email=" + email + "]";
 	}
 }
