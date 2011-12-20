8410a5d2ce008e3605fd270623af011af1677a70
subaochen@gmail.com <subaochen@gmail.com@25014077-2549-54d7-ebe6-9a0042c0366a>
2011-12-20 23:59:30 +0000

    准备实现编辑商品

--- trunk/src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
+++ trunk/src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
@@ -11,13 +11,45 @@ import javax.persistence.PersistenceContext;
 import cn.edu.sdut.openeshop.model.Goods;
 
 @Stateless
+@Named
 public class GoodsManager {
 	@PersistenceContext
 	private EntityManager em;
 	
+	private Goods instance;
+	
+	public GoodsManager(){
+		if(instance == null) instance = new Goods();
+		Long id = instance.getId();
+		if(id != null && id != 0) instance = loadInstance(id);
+	}
+	
+	private Goods loadInstance(Long id) {
+		return em.find(Goods.class, id);
+	}
+
 	@Produces @Named
 	public List<Goods> getAllGoods(){
 		return em.createQuery("from Goods goods").getResultList();
 	}
+	
+	public void persist(){
+		em.persist(instance);
+	}
+	
+	public void update(){
+		em.merge(instance);
+	}
 
+	public Goods getInstance() {
+		return instance;
+	}
+
+	public void setInstance(Goods instance) {
+		this.instance = instance;
+	}
+	
+	public boolean isManaged(){
+		return instance != null & instance.getId() != null && instance.getId() != 0;
+	}
 }
