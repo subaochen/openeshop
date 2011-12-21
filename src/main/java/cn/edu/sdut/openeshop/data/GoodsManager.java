18a7591a721634170e4bfee399d57b9a841ee199
subaochen <subaochen@gmail.com>
2011-12-21 09:38:08 +0000

    尝试处理page action & page param，似乎应该采用JSF2的策略

--- src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
+++ src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
@@ -2,6 +2,7 @@ package cn.edu.sdut.openeshop.data;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
@@ -9,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import cn.edu.sdut.openeshop.model.Goods;
+import cn.edu.sdut.openeshop.tools.HttpParam;
 
 @Stateless
 @Named
@@ -18,7 +20,10 @@ public class GoodsManager {
 	
 	private Goods instance;
 	
+	@HttpParam("goodsId") String goodsId;
+	
 	public GoodsManager(){
+		System.out.println("goodsId=" + goodsId);
 		if(instance == null) instance = new Goods();
 		Long id = instance.getId();
 		if(id != null && id != 0) instance = loadInstance(id);
@@ -51,5 +56,10 @@ public class GoodsManager {
 	
 	public boolean isManaged(){
 		return instance != null & instance.getId() != null && instance.getId() != 0;
+	}
+	
+	@PostConstruct
+	public void postConstruct(){
+		System.out.println("goods id =" + goodsId);
 	}
 }
