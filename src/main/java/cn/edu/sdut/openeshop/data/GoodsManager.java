c786b15a974df8f251705086b58c453a87d25280
subaochen <subaochen@gmail.com>
2011-12-22 23:53:21 +0000

    增加商品的搜索功能

--- src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
+++ src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
@@ -1,5 +1,6 @@
 package cn.edu.sdut.openeshop.data;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -12,6 +13,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import cn.edu.sdut.openeshop.model.Goods;
 
@@ -21,40 +23,62 @@ import cn.edu.sdut.openeshop.model.Goods;
 public class GoodsManager {
 	@PersistenceContext
 	private EntityManager em;
-	
-	@Inject Logger log;
+
+	@Inject
+	Logger log;
+
+	private List<Goods> resultList = new ArrayList<Goods>(0);
 
 	private Goods instance = new Goods();
 	private Long goodsId;
-	
-	public void wire(){
+	private String searchFor;
+
+	public void wire() {
 		log.info("GoodsManager wire is called,goodsId=" + goodsId);
-		if(getGoodsId() != null && getGoodsId() != 0) 
+		if (getGoodsId() != null && getGoodsId() != 0)
 			loadInstance(getGoodsId());
 	}
-	
+
 	private void loadInstance(Long id) {
 		instance = em.find(Goods.class, id);
 	}
 
-	@Produces @Named
-	public List<Goods> getAllGoods(){
+	public List<Goods> getResultList() {
+		log.info("searchFor=" + searchFor);
+		if (resultList.size() != 0)
+			return resultList;
+		if (searchFor != null)
+			return em
+					.createQuery(
+							"from Goods goods where lower(name) like lower(concat(concat('%',:name),'%'))")
+					.setParameter("name", searchFor).getResultList();
 		return em.createQuery("from Goods goods").getResultList();
 	}
-	
-	public String save(){
+
+	/**
+	 * 根据名称搜索
+	 * 
+	 * @return
+	 */
+	public String search() {
+		return "/admin/goods_list.jsf";
+	}
+
+	public String save() {
 		em.merge(instance);
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("成功保存商品信息"));
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage("成功保存商品信息"));
 		return "/admin/goods_list.jsf";
 	}
-	
-	public String remove(Long id){
+
+	public String remove(Long id) {
 		em.remove(findGoods(id));
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("成功删除商品信息"));
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage("成功删除商品信息"));
 		return "/admin/goods_list.jsf";
 	}
-	
-	public Goods findGoods(Long id){
+
+	public Goods findGoods(Long id) {
 		return em.find(Goods.class, id);
 	}
 
@@ -72,5 +96,13 @@ public class GoodsManager {
 
 	public void setGoodsId(Long goodsId) {
 		this.goodsId = goodsId;
+	}
+
+	public String getSearchFor() {
+		return searchFor;
+	}
+
+	public void setSearchFor(String searchFor) {
+		this.searchFor = searchFor;
 	}
 }
