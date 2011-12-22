b99180fc881b2a1257a3aac1c7aaff54c241c78d
subaochen@gmail.com <subaochen@gmail.com@25014077-2549-54d7-ebe6-9a0042c0366a>
2011-12-22 08:04:38 +0000

    完成商品的列表、添加、删除和编辑功能

--- trunk/src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
+++ trunk/src/main/java/cn/edu/sdut/openeshop/data/GoodsManager.java
@@ -1,32 +1,34 @@
 package cn.edu.sdut.openeshop.data;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.ejb.Stateful;
-import javax.ejb.Stateless;
-import javax.enterprise.context.Dependent;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import cn.edu.sdut.openeshop.model.Goods;
 
-@Stateless
+@Stateful
+@RequestScoped
 @Named
 public class GoodsManager {
 	@PersistenceContext
 	private EntityManager em;
 	
+	@Inject Logger log;
+
 	private Goods instance = new Goods();
 	private Long goodsId;
 	
-	
 	public void wire(){
-		System.out.println("call goodsManager wire,goodsId="+goodsId);
+		log.info("GoodsManager wire is called,goodsId=" + goodsId);
 		if(getGoodsId() != null && getGoodsId() != 0) 
 			loadInstance(getGoodsId());
 	}
@@ -40,16 +42,20 @@ public class GoodsManager {
 		return em.createQuery("from Goods goods").getResultList();
 	}
 	
-	public String persist(){
-		em.persist(instance);
+	public String save(){
+		em.merge(instance);
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("成功保存商品信息"));
 		return "/admin/goods_list.jsf";
 	}
 	
-	public String update(){
-		em.merge(instance);
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("成功更新商品信息"));
+	public String remove(Long id){
+		em.remove(findGoods(id));
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("成功删除商品信息"));
 		return "/admin/goods_list.jsf";
+	}
+	
+	public Goods findGoods(Long id){
+		return em.find(Goods.class, id);
 	}
 
 	public Goods getInstance() {
@@ -58,10 +64,6 @@ public class GoodsManager {
 
 	public void setInstance(Goods instance) {
 		this.instance = instance;
-	}
-	
-	public boolean isManaged(){
-		return instance != null && instance.getId() != null && instance.getId() != 0;
 	}
 
 	public Long getGoodsId() {
