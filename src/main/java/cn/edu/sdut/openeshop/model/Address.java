639082d68293a1e6c8ad3c6e52b02843450549e7
subaochen <subaochen@gmail.com>
2012-02-09 09:07:27 +0000

    完成提交订单功能，不过为什么不能执行conversation.end(ChekcoutBean.java)呢？

---
+++ src/main/java/cn/edu/sdut/openeshop/model/Address.java
@@ -0,0 +1,31 @@
+package cn.edu.sdut.openeshop.model;
+
+public class Address {
+	
+	private String addr;
+	private String addrName;
+	private String addrTel;
+	
+	public Address() {
+	}
+	
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getAddrName() {
+		return addrName;
+	}
+	public void setAddrName(String addrName) {
+		this.addrName = addrName;
+	}
+	public String getAddrTel() {
+		return addrTel;
+	}
+	public void setAddrTel(String addrTel) {
+		this.addrTel = addrTel;
+	}
+	
+}
